2c0e5bd52295d09dc858e59f66651ba2172e2263
JessicaPersou <jessica.sousa@koin.com.br>
2022-07-21 20:32:27 -0300

    Brincando com Códigos

--- Introdução Node/operadores.js
+++ Introdução Node/operadores.js
@@ -15,24 +15,21 @@ let concatenar =  (valor1  ,  valor2)
 valor1++
 valor2--
 
-console.log( "OPERAÇÕES DE EXPRESSÃO:  ")
+console.log( "\nOPERAÇÕES DE EXPRESSÃO:  ")
+console.log( "Soma -> 5 + 5 = " ,  soma) 
+console.log(  "Subtração -> 5 - 5 = "  ,  sub)
+console.log(  "Divisão -> 5 / 5 = "  , div)
+console.log(  "Multiplicação -> 5 * 5 = "  ,  multi)
+console.log(  "Resto da Div -> 81 % 5 = "  ,  resto)
+console.log( "Exponenciação -> 5 ** 5 = " ,  expo)
+console.log( "Concatenar -> 5  , 5 = " ,  concatenar)
+console.log( "Incremento -> 5  = " ,  valor1)
+console.log( "Decremento -> 5  = " , valor2)
 
-console.log( "Soma -> 5 + 5 = " ,  soma  ) 
-console.log(  "Subtração -> 5 - 5 = "  ,  sub   )
-console.log(  "Divisão -> 5 / 5 = "  , div   )
-console.log(  "Multiplicação -> 5 * 5 = "  ,  multi  )
-console.log(  "Resto da Div -> 81 % 5 = "  ,  resto  )
-console.log( "Exponenciação -> 5 ** 5 = " ,  expo  )
-console.log( "Concatenar -> 5  , 5 = " ,  concatenar  )
-console.log( "Incremento -> 5  = " ,  valor1 )
-console.log( "Decremento -> 5  = " , valor2  )
 
-
-console.log( "OPERADORES DE COMPARAÇÃO:  ") // pode ser usador para comparar qualquer tipo de valor, string, array, float e int ,
+console.log( "\nOPERADORES DE COMPARAÇÃO:  ") // pode ser usador para comparar qualquer tipo de valor, string, array, float e int ,
 let valores1 = 5
 let valores2 = 5
-
-
 
 if( valor1 ===  valor2){
     console.log( "Identico (===) ")
@@ -63,7 +60,7 @@ if( valor1 <=  v1){
 }
 
 
-console.log( "OPERADORES DE LÓGICOS:  ") 
+console.log( "\nOPERADORES DE LÓGICOS:  ") 
 
 if( valor1 ==  valor2 &&  v1 >  v2){
     console.log( "Operador AND && (Dois valores tem que ser verdadeiros!) ")
@@ -75,29 +72,18 @@ if(! v1 <  v2){
     console.log( "Operador NOT ! (Valores Falsos!) ")
 }
 
-console.log( "OPERADORES CAST:  ") // ele força a conversão de dados seja qual for 
-
-
-
+console.log( "\nOPERADORES CAST:  ") // ele força a conversão de dados seja qual for 
 if( valor1 ===  v2){
     console.log( "Operador de Cast verificando se é identico!")
 }
 
-
-console.log( "OPERADORES DE ATRIBUIÇÃO:  ")
-
+console.log( "\nOPERADORES DE ATRIBUIÇÃO:  ")
 console.log( "Soma += "  ,  soma) // como se fosse o número operado pelo operador dele novamente 
-
 console.log( "Subtração -= "  ,  sub)
-
 console.log( "Divisão /= "  ,  div)
-
 console.log( "Multiplicação *= "  ,  multi)
-
 console.log( "Resto da divisão %= "  ,  resto)
 
-
-console.log( "OPERADORES DE TERNÁRIO:  ") // pode ser usado com qualquer valor
+console.log( "\nOPERADORES DE TERNÁRIO:  ") // pode ser usado com qualquer valor
 console.log( 20 > 10 ? "Verdadeiro": "Falso")
-
 console.log( 20 == 10 ? "Verdadeiro": "Falso")
---
+++ Introdução Node/teste.js
@@ -0,0 +1,7 @@
+let alunos = ["João", "Pedro", "Jorge", "Francisco"]
+
+indiceJoao = alunos.indexOf("João")
+console.log(indiceJoao)
+indiceFrancisco = alunos.indexOf("Francisco")
+
+console.log(indiceFrancisco)
