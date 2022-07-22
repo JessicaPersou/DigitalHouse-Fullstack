188b7997e846d52ec2e5cb3dee24e7fdb74f3aa7
JessicaPersou <jessica.sousa@koin.com.br>
2022-07-22 09:17:25 -0300

    Encerramento da aula

---
+++ Introdução Node/votar.js
@@ -0,0 +1,9 @@
+let idade1 = 18
+let idade2 = 65
+let idadePessoa = 14
+
+if( idadePessoa >= idade1 || idadePessoa <= idade2){
+    console.log("Voto é Obrigatório!")
+}else{
+    console.log("Voto é Opcional!")
+}
