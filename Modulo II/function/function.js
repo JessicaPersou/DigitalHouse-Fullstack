12fded17c3f072b627609455bd9a8a855e19142c
JessicaPersou <jessica.sousa@koin.com.br>
2022-08-05 18:13:27 -0300

    Adicionando exercícios de funções

---
+++ Modulo II/function/function.js
@@ -0,0 +1,21 @@
+function triplo (number){
+    return 3*number
+}
+
+let n = 5
+
+triplo(n);
+
+//
+
+// function calcularValorDevido(pesoDaRoupaSuja){
+//     return pesoDaRoupaSuja * 5
+// }
+
+// console.log(calcularValorDevido(5))
+
+// //
+function calcularValorDevido(pesoDeRoupaSuja){
+    return (pesoDeRoupaSuja * 3.00) + 10.0  
+}
+console.log(calcularValorDevido(3))
