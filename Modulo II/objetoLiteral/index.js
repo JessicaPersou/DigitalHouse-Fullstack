d8cfd482d223a7c9c2e232bd43205f180fe95af5
JessicaPersou <jessica.sousa@koin.com.br>
2022-08-09 09:09:26 -0300

    objeto literal

---
+++ Modulo II/objetoLiteral/index.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    nome : "Flávio",
+    idade : 19,
+    curso : "ADS"
+};
+
+console.log(pessoa);
+console.log(pessoa.nome);
+console.log(pessoa["idade"]);
+
+// métodos nosobjetos literais são funções que atribuem um valor a uma chave 
