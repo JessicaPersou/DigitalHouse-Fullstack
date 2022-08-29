959ec62f49662f5a4c143f62263126bd83805182
Jessica P Sousa <jessica.persou@gmail.com>
2022-08-28 21:11:05 -0300

    adicionando estudo de domingo

---
+++ Modulo III/EcommerceDH/arrowFunction.js
@@ -0,0 +1,30 @@
+/*let dobro = (num) => num * 2;
+
+let triplo = (num) => num * 3;
+
+let aplicar = (num, dobro) => {
+ return aplicar;
+}
+
+console.log(aplicar(2,dobro))
+
+function dobro(num1) {
+    num1 * 2;
+}
+
+function triplo(num1) {
+    num1 * 3;
+}
+
+let aplicar = (num, dobro) => {
+    return aplicar;
+}
+
+console.log(aplicar(5, dobro));*/
+
+let frase = "Essa semana vou no colearning";
+let ar = frase.split(" ")
+
+for(let f of ar ){
+    console.log(f)
+}
