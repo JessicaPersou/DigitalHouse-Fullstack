c20c0e4e4b355c2a66c9dd0be33bf876cdb31c67
JessicaPersou <jessica.sousa@koin.com.br>
2022-09-06 18:18:12 -0300

    estudando rotas

--- Modulo I/Revisão/testeArray.js
+++ Modulo I/Revisão/testeArray.js
@@ -1,12 +1,26 @@
-let sabores = ["Morango"]
+// A função a seguir (calculaIdade) não precisa ser alterada
+// A sua função deve ser escrita logo abaixo desta
+function calcularIdade(dataDeNascimento) {
+    let [dia, mes, ano] = dataDeNascimento.split('/');
+    const d = new Date();
+    const anoAtual = d.getFullYear();
+    const mesAtual = d.getMonth() + 1;
+    const diaAtual = d.getDate();
+    ano = +ano; mes = +mes; dia = +dia;
+    let quantosAnos = anoAtual - ano;
+    if (mesAtual < mes || mesAtual == mes && diaAtual < dia) {
+        quantosAnos--;
+    }
+    return quantosAnos < 0 ? 0 : quantosAnos;
+ }
+  
+ // Escreva aqui sua função
+ function deixaEntrar(dataDeNascimento, censura){
+     if(dataDeNascimento > censura){
+         return true
+     }else{
+         return false
+     }
+ }
 
-function agregandoSabor(novo, atual){
-    novoSabor = sabores
-    atual = novoSabor.push(novo)
-    
-    return atual
-}
-
-agregandoSabor(["Baunilha","Chocolate"], sabores)
-
-console.log(sabores)
+ console.log(deixaEntrar())
